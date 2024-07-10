03bbcdf9abdede4230283896e9bebfb56a6f0b77
sean park <rookiejj@gmail.com>
2024-07-10 17:09:58 +0900

    initializing project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>우산 가져가!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
